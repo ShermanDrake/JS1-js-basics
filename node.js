5e7e688a50d4dadd7dbe284ae3bfdc881f3a89c7
ShermanDrake <shermandrake@comcast.net>
2015-09-07 11:56:58 -0600

    first commit

---
+++ node.js
@@ -0,0 +1,44 @@
+//console.log(-20)
+
+//var result = 10 / 3
+//console.log(result)
+
+//"hello"
+
+//var greeting = "hellow" + "world"
+//console.log(greeting)
+
+//var math = "1 + 1"
+//console.log(math)
+
+//alert('Hello Boulder!');
+
+//console.log(10 > 3)
+
+if ( true ) {
+	console.log("it's true!")
+}
+
+var canDrink
+
+var age = prompt("how old are you?")
+
+var country = 'Mexico'
+
+console.log( typeof age)
+
+if ( +age >= 21 ) {
+	canDrink = true
+
+}
+
+else if ( (country === 'Mexico' ) && ( +age >= 18) ) {
+
+}
+
+else {
+	canDrink = false
+
+}
+
+console.log('will you be served? ' + canDrink)
